eeb48ccfae6bc8222fedfb023074b55b263f753d
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 18:27:19 +0530

    delete a variable and add three new varaiables and set output

--- src/practice1/StringHandle9.java
+++ src/practice1/StringHandle9.java
@@ -6,14 +6,19 @@ public class StringHandle9 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String inputString = scan.nextLine().trim();
-        int sum = 0;
+        int upperWeight = 0;
+        int lowerWeight = 0;
+        int absoluteWeight;
         for (int i = 0; i < inputString.length(); i++) {
             char c = inputString.charAt(i);
             if (c > 64 && c < 92) {
-                sum = sum + inputString.charAt(i);
+                upperWeight = upperWeight + inputString.charAt(i);
+            } else if (c >= 92 && c < 127) {
+                lowerWeight = lowerWeight + inputString.charAt(i);
             }
         }
-        System.out.print(sum);
+        absoluteWeight = upperWeight - lowerWeight;
+        System.out.print("Absolute Weight is " + absoluteWeight);
         scan.close();
     }
 }
