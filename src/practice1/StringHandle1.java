e3b60c670e8a9fa491f3cd48b41b0c64e79f1538
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 17:20:26 +0530

    take input from user and print the output

--- src/practice1/StringHandle1.java
+++ src/practice1/StringHandle1.java
@@ -5,5 +5,15 @@ import java.util.Scanner;
 public class StringHandle1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String string1 = scan.nextLine();
+        System.out.print("Index : ");
+        for (int i = 0; i < string1.length(); i++) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n" + "Chars : ");
+        for (int i = 0; i < string1.length(); i++) {
+            System.out.print(string1.charAt(i) + " ");
+        }
+        scan.close();
     }
 }
