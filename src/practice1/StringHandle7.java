368b1151481de499c9ac57fb398ee67674421887
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 17:54:51 +0530

    take input from user and print the output

--- src/practice1/StringHandle7.java
+++ src/practice1/StringHandle7.java
@@ -5,5 +5,12 @@ import java.util.Scanner;
 public class StringHandle7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine().trim();
+        int sum = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            sum = sum + inputString.charAt(i);
+        }
+        System.out.println(sum);
+        scan.close();
     }
 }
