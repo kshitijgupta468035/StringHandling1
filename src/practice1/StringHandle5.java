68ebeec56c28d92f1d7890df1cfde360ad6e3a48
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 17:40:33 +0530

    Add Scanner class

--- src/practice1/StringHandle5.java
+++ src/practice1/StringHandle5.java
@@ -1,4 +1,9 @@
 package practice1;
 
+import java.util.Scanner;
+
 public class StringHandle5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
 }
