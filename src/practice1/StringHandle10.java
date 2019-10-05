bb2b0086cc6930ee66f933ba8bee6024bdb375e7
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 18:02:12 +0530

    add main method and Scanner class

--- src/practice1/StringHandle10.java
+++ src/practice1/StringHandle10.java
@@ -1,4 +1,9 @@
 package practice1;
 
+import java.util.Scanner;
+
 public class StringHandle10 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
 }
