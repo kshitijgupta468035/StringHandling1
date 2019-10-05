da23d2c81aebfed52e455480da5615b975ffddac
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 18:09:41 +0530

    Taking input from user and printing output

--- src/practice1/StringHandle11.java
+++ src/practice1/StringHandle11.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class StringHandle11 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine().trim();
+        char ch = scan.nextLine().trim().charAt(0);
+        for (int i = 0; i < inputString.length(); i++) {
+            //char c = inputString.charAt(i);        // also compare the ch with c by making creating char c
+            if (inputString.charAt(i) == ch) {
+                System.out.println(i);
+            }
+        }
     }
 }
