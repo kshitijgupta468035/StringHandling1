0852496da4d73eaea22b7a2a6595e12757c21dc6
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 17:46:27 +0530

    print the output after taking input from user

--- src/practice1/StringHandle6.java
+++ src/practice1/StringHandle6.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class StringHandle6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine().trim();
+        //inputString = inputString.toUpperCase();  Another way to print the Output
+        //System.out.println(inputString);
+        System.out.println(inputString.toUpperCase());
     }
 }
