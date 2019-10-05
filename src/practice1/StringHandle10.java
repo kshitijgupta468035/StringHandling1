7020e4267497bf3df01adfefb27a1dd8f964c325
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 18:03:19 +0530

    take input and print the output

--- src/practice1/StringHandle10.java
+++ src/practice1/StringHandle10.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class StringHandle10 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine().trim();
+        char character = scan.nextLine().trim().charAt(0);
+        System.out.println(inputString.indexOf(character));
     }
 }
