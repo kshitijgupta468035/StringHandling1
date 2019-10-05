9440ca2552cb8446654f99dfa9324d6ec713a669
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2019-10-05 17:33:26 +0530

    take input from user and print the output

--- src/practice1/StringHandle3.java
+++ src/practice1/StringHandle3.java
@@ -5,5 +5,17 @@ import java.util.Scanner;
 public class StringHandle3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine().trim();
+        char[] array = inputString.toCharArray();
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                array[i] = '*';
+            }
+        }
+        for (int i = 0; i < inputString.length(); i++) {
+            System.out.print(array[i]);
+        }
+        scan.close();
     }
 }
